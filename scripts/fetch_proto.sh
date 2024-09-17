7fa6824b19dffa51470aadba0a3b340f5b29f79e
Manik Jain <mjain@jumptrading.com>
2024-09-17 02:00:01 +0000

    More

--- scripts/fetch_proto.sh
+++ scripts/fetch_proto.sh
@@ -2,10 +2,7 @@
 
 # Fetch protosol (TODO: take out checkout once protosol branch is merged)
 if [ ! -d protosol ]; then
-  git clone --depth=1 -b  -q https://github.com/firedancer-io/protosol.git
-  cd protosol
-  git checkout runtime_fuzz_v2_updates
-  cd ..
+  git clone --depth=1 -b runtime_fuzz_v2_updates -q https://github.com/firedancer-io/protosol.git
 else
   cd protosol
   git pull -q
