03df4910a2f28169674db21172ff12ce4e45b328
Emanuele Cesena <ecesena@jumptrading.com>
2024-10-08 17:48:31 -0500

    syscalls: fix stack size for real

--- src/utils/vm/mod.rs
+++ src/utils/vm/mod.rs
@@ -1,5 +1,10 @@
 pub mod err_map;
 pub mod mem_regions;
 
-pub const STACK_SIZE: usize = 4096;
+/* Note: these contants are different for Firedancer vs Agave.
+For both, the virtual stack is 64x pages of 4kB each, with 4kB gaps.
+- Firedancer allocates 64 * (4+4)kB and wastes, i.e. gaps are phisical.
+- Agave allocates the 64 * 4kB, i.e. gaps are virtual and handled by vm_to_host(). */
+pub const STACK_SIZE: usize = 64 * 4_096;
+pub const STACK_GAP_SIZE: u64 = 4_096;
 pub const HEAP_MAX: usize = 256 * 1024;
