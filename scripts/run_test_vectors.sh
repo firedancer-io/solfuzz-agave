bbe45fea901399ee70cbf53912e53edab8b017c3
Emanuele Cesena <ecesena@jumptrading.com>
2025-01-03 23:00:07 +0000

    vm interp: remove process_result (unnecessary)

--- scripts/run_test_vectors.sh
+++ scripts/run_test_vectors.sh
@@ -31,10 +31,10 @@ find dump/test-vectors/txn/fixtures/precompile/secp256k1 -type f -name '*.fix' |
 find dump/test-vectors/txn/fixtures/programs -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_txn {} > $LOG_PATH/test_exec_txn.log 2>&1
 find dump/test-vectors/cpi/fixtures -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_cpi {} > $LOG_PATH/test_exec_cpi.log 2>&1
 find dump/test-vectors/syscall/fixtures -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_syscall {} > $LOG_PATH/test_exec_vm_syscall.log 2>&1
-# find dump/test-vectors/vm_interp/fixtures/latest -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
-# find dump/test-vectors/vm_interp/fixtures/v0 -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
-# find dump/test-vectors/vm_interp/fixtures/v1 -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
-# find dump/test-vectors/vm_interp/fixtures/v2 -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
+find dump/test-vectors/vm_interp/fixtures/latest -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
+find dump/test-vectors/vm_interp/fixtures/v0 -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
+find dump/test-vectors/vm_interp/fixtures/v1 -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
+find dump/test-vectors/vm_interp/fixtures/v2 -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
 find dump/test-vectors/elf_loader/fixtures -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_elf_loader {} > $LOG_PATH/test_exec_elf_loader.log 2>&1
 
 failed=`grep -wR FAIL $LOG_PATH | wc -l`
