e425c216075a2309ca6a1bc71002bb1b3576240e
Manik Jain <mjain@jumptrading.com>
2025-02-18 15:58:30 +0000

    Remove zk token from supported features list

--- src/lib.rs
+++ src/lib.rs
@@ -215,7 +215,7 @@ static SUPPORTED_FEATURES: &[u64] = feature_list![
     pico_inflation,
     warp_timestamp_again,
     blake3_syscall_enabled,
-    zk_token_sdk_enabled,
+    // zk_token_sdk_enabled, // NOT supported in fd
     curve25519_syscall_enabled,
     enable_partitioned_epoch_reward,
     stake_raise_minimum_delegation_to_1_sol,
