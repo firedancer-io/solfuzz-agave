2d45c48595cfde0782df6320beb87c5e5c401ef5
mjain-jump <150074777+mjain-jump@users.noreply.github.com>
2025-03-20 16:44:39 -0500

    * Reduce num processes for CI (#239)
    * Add back secp256r1

--- scripts/run_test_vectors.sh
+++ scripts/run_test_vectors.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+NUM_PROCESSES=${NUM_PROCESSES:-8}
+
 if [ "$LOG_PATH" == "" ]; then
   LOG_PATH="`mktemp -d`"
 else
@@ -22,19 +24,16 @@ else
   cd ../..
 fi
 
-find dump/test-vectors/instr/fixtures -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_instr {} > $LOG_PATH/test_exec_instr.log 2>&1
-# secp256r1 currently not working, agave has bugs
-# find dump/test-vectors/txn/fixtures/precompile -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_txn {} > $LOG_PATH/test_exec_precompile.log 2>&1
-find dump/test-vectors/txn/fixtures/precompile/ed25519 -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_txn {} > $LOG_PATH/test_exec_precompile.log 2>&1
-find dump/test-vectors/txn/fixtures/precompile/secp256k1 -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_txn {} > $LOG_PATH/test_exec_precompile.log 2>&1
-find dump/test-vectors/txn/fixtures/programs -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_txn {} > $LOG_PATH/test_exec_txn.log 2>&1
-find dump/test-vectors/cpi/fixtures -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_cpi {} > $LOG_PATH/test_exec_cpi.log 2>&1
-find dump/test-vectors/syscall/fixtures -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_syscall {} > $LOG_PATH/test_exec_vm_syscall.log 2>&1
-find dump/test-vectors/vm_interp/fixtures/latest -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
-find dump/test-vectors/vm_interp/fixtures/v0 -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
-find dump/test-vectors/vm_interp/fixtures/v1 -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
-find dump/test-vectors/vm_interp/fixtures/v2 -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
-find dump/test-vectors/elf_loader/fixtures -type f -name '*.fix' | xargs -P 32 -I {} ./target/release/test_exec_elf_loader {} > $LOG_PATH/test_exec_elf_loader.log 2>&1
+find dump/test-vectors/instr/fixtures -type f -name '*.fix' | xargs -P $NUM_PROCESSES -I {} ./target/release/test_exec_instr {} > $LOG_PATH/test_exec_instr.log 2>&1
+find dump/test-vectors/txn/fixtures/precompile -type f -name '*.fix' | xargs -P $NUM_PROCESSES -I {} ./target/release/test_exec_txn {} > $LOG_PATH/test_exec_precompile.log 2>&1
+find dump/test-vectors/txn/fixtures/programs -type f -name '*.fix' | xargs -P $NUM_PROCESSES -I {} ./target/release/test_exec_txn {} > $LOG_PATH/test_exec_txn.log 2>&1
+find dump/test-vectors/cpi/fixtures -type f -name '*.fix' | xargs -P $NUM_PROCESSES -I {} ./target/release/test_exec_cpi {} > $LOG_PATH/test_exec_cpi.log 2>&1
+find dump/test-vectors/syscall/fixtures -type f -name '*.fix' | xargs -P $NUM_PROCESSES -I {} ./target/release/test_exec_vm_syscall {} > $LOG_PATH/test_exec_vm_syscall.log 2>&1
+find dump/test-vectors/vm_interp/fixtures/latest -type f -name '*.fix' | xargs -P $NUM_PROCESSES -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
+find dump/test-vectors/vm_interp/fixtures/v0 -type f -name '*.fix' | xargs -P $NUM_PROCESSES -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
+find dump/test-vectors/vm_interp/fixtures/v1 -type f -name '*.fix' | xargs -P $NUM_PROCESSES -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
+find dump/test-vectors/vm_interp/fixtures/v2 -type f -name '*.fix' | xargs -P $NUM_PROCESSES -I {} ./target/release/test_exec_vm_interp {} > $LOG_PATH/test_exec_vm_interp.log 2>&1
+find dump/test-vectors/elf_loader/fixtures -type f -name '*.fix' | xargs -P $NUM_PROCESSES -I {} ./target/release/test_exec_elf_loader {} > $LOG_PATH/test_exec_elf_loader.log 2>&1
 
 failed=`grep -wR FAIL $LOG_PATH | wc -l`
 passed=`grep -wR OK $LOG_PATH | wc -l`
