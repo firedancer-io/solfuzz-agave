63fe2a98501ebcc4555a0493b8213ad49389cd1c
Manik Jain <mjain@jumptrading.com>
2025-04-28 22:56:58 +0000

    Block fuzzing target

--- src/txn_fuzzer.rs
+++ src/txn_fuzzer.rs
@@ -1,5 +1,6 @@
 use crate::proto::{self, ResultingState};
-use crate::proto::{AcctState, TransactionMessage, TxnContext, TxnResult};
+use crate::proto::{AcctState, TxnContext, TxnResult};
+use crate::utils::program::common::{build_versioned_message, get_dummy_bpf_native_programs};
 use crate::TOGGLE_DIRECT_MAPPING;
 use agave_feature_set::*;
 use ahash::AHashSet;
@@ -10,7 +11,7 @@ use solana_accounts_db::accounts_index::{AccountsIndexConfig, IndexLimitMb};
 use solana_program::hash::Hash;
 use solana_program::instruction::CompiledInstruction;
 use solana_program::message::v0::MessageAddressTableLookup;
-use solana_program::message::{legacy, v0, MessageHeader, VersionedMessage};
+use solana_program::message::MessageHeader;
 use solana_program::pubkey::Pubkey;
 use solana_runtime::account_saver::collect_accounts_for_failed_tx;
 use solana_runtime::bank::{Bank, LoadAndExecuteTransactionsOutput};
@@ -24,11 +25,11 @@ use solana_sdk::message::SanitizedMessage;
 use solana_sdk::precompiles::get_precompile;
 use solana_sdk::rent::Rent;
 use solana_sdk::signature::Signature;
+use solana_sdk::sysvar;
 use solana_sdk::transaction::{
     TransactionError, TransactionVerificationMode, VersionedTransaction,
 };
 use solana_sdk::transaction_context::TransactionAccount;
-use solana_sdk::{bpf_loader_upgradeable, sysvar};
 use solana_svm::account_loader::LoadedTransaction;
 use solana_svm::runtime_config::RuntimeConfig;
 use solana_svm::transaction_error_metrics::TransactionErrorMetrics;
@@ -111,61 +112,6 @@ impl From<&proto::MessageAddressTableLookup> for MessageAddressTableLookup {
                 .map(|idx| *idx as u8)
                 .collect(),
         }
-    }
-}
-
-fn build_versioned_message(value: &TransactionMessage) -> Option<VersionedMessage> {
-    let header = if let Some(value_header) = value.header {
-        MessageHeader::from(&value_header)
-    } else {
-        // Default: valid txn header with 1 signature (this keeps tests simpler)
-        MessageHeader {
-            num_required_signatures: 1,
-            num_readonly_signed_accounts: 0,
-            num_readonly_unsigned_accounts: 0,
-        }
-    };
-    let account_keys = value
-        .account_keys
-        .iter()
-        .map(|key| Pubkey::new_from_array(key.clone().try_into().unwrap()))
-        .collect::<Vec<Pubkey>>();
-    let recent_blockhash = if value.recent_blockhash.is_empty() {
-        // Default: empty blockchash (this keeps tests simpler)
-        Hash::new_from_array([0u8; 32])
-    } else {
-        Hash::new_from_array(value.recent_blockhash.clone().try_into().unwrap())
-    };
-    let instructions = value
-        .instructions
-        .iter()
-        .map(CompiledInstruction::from)
-        .collect::<Vec<CompiledInstruction>>();
-
-    if value.is_legacy {
-        let message = legacy::Message {
-            header,
-            account_keys,
-            recent_blockhash,
-            instructions,
-        };
-        Some(VersionedMessage::Legacy(message))
-    } else {
-        let address_table_lookups = value
-            .address_table_lookups
-            .iter()
-            .map(MessageAddressTableLookup::from)
-            .collect::<Vec<MessageAddressTableLookup>>();
-
-        let message = v0::Message {
-            header,
-            account_keys,
-            recent_blockhash,
-            instructions,
-            address_table_lookups,
-        };
-
-        Some(VersionedMessage::V0(message))
     }
 }
 
@@ -373,19 +319,6 @@ fn output_txn_result_from_result(
             prioritization_fee: fees.prioritization_fee(),
         }),
     }
-}
-
-fn get_dummy_bpf_native_programs() -> Vec<(Pubkey, AccountSharedData)> {
-    vec![
-        (
-            solana_sdk::address_lookup_table::program::id(),
-            AccountSharedData::new(1u64, 0, &bpf_loader_upgradeable::id()),
-        ),
-        (
-            solana_sdk::config::program::id(),
-            AccountSharedData::new(1u64, 0, &bpf_loader_upgradeable::id()),
-        ),
-    ]
 }
 
 #[allow(deprecated)]
