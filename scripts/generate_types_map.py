0b7cfeb9988a5bb04ea25c8b349f4df3e94772b4
jumpsiegel <83408952+jumpsiegel@users.noreply.github.com>
2025-06-30 08:48:38 -0500

    type map changed again (#308)

--- scripts/generate_types_map.py
+++ scripts/generate_types_map.py
@@ -1,6 +1,6 @@
 # Example usage:
 
-# python3 scripts/generate_types_map.py ../firedancer/src/flamenco/types/{fd_types.json,fd_types_reflect_generated.c} src/types/types_map_generated.rs
+# python3 scripts/generate_types_map.py --print-whitelist-idx ../firedancer/src/flamenco/types/{fd_types.json,fd_types_reflect_generated.c} src/types/types_map_generated.rs > ../solfuzz/src/mutate/whitelist.h
 
 import argparse
 import json
