b6db6c7bd580c4cf7285f801973054041daf569b
Manik Jain <mjain@jumptrading.com>
2025-08-04 18:13:33 +0000

    feature: hardcode remove_accounts_executable_flag_checks for fuzzing

--- src/lib.rs
+++ src/lib.rs
@@ -269,7 +269,8 @@ pub static HARDCODED_FEATURES: &[u64] = feature_list![
     raise_block_limits_to_50m,
     move_precompile_verification_to_svm,
     enable_transaction_loading_failure_fees,
-    enable_loader_v4, // custom hardcoded feature
+    enable_loader_v4,                       // custom hardcoded feature
+    remove_accounts_executable_flag_checks, // custom hardcoded feature
 ];
 
 static SUPPORTED_FEATURES: &[u64] = feature_list![
@@ -297,7 +298,6 @@ static SUPPORTED_FEATURES: &[u64] = feature_list![
     enable_sbpf_v3_deployment_and_execution,
     // migrate_stake_program_to_core_bpf, // NOT impl in fd
     enable_get_epoch_stake_syscall,
-    remove_accounts_executable_flag_checks,
     fix_alt_bn128_multiplication_input_length,
     lift_cpi_caller_restriction,
     accounts_lt_hash,
