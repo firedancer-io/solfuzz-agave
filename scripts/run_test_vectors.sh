4275d798bfb7829cc9291bd6bab9bea5033042d4
mjain-jump <150074777+mjain-jump@users.noreply.github.com>
2026-06-08 16:43:27 -0500

    shred: add initial test vectors (#537)

--- scripts/run_test_vectors.sh
+++ scripts/run_test_vectors.sh
@@ -117,6 +117,7 @@ declare -A JOBS=(
   [test_exec_block]="dump/test-vectors/block/fixtures"
   [test_exec_vm_syscall]="dump/test-vectors/syscall/fixtures"
   [test_exec_elf_loader]="dump/test-vectors/elf_loader/fixtures"
+  [test_exec_shred]="dump/test-vectors/shred/fixtures"
 )
 
 any_job_failed=0
